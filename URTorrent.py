ef47a3a2f2e436f39d1c35754aa855a5d913fb25
Avi Webberman <aviwebberman@dhcp-10-5-30-246.wireless.rochester.edu>
2017-11-28 08:42:36 -0500

    metainfo
    added metainfo method mostly

--- URTorrent.py
+++ URTorrent.py
@@ -72,6 +72,7 @@ def main():
 		print("pieces' hashes:  ")
 		num_pieces = math.ceil(info_dict[b"length"]/info_dict[b"piece length"])
 		print(num_pieces)
+		print("other")
 	#	for i in range(0, num_pieces):
 	#		print(info_dict[b"pieces"][i:i+20+1].hexdigest())
 		
