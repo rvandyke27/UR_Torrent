fcc2efb71ca2ccbb24aae802f47774eb5f0a8913
rvandyke27 <rvandyke27@gmail.com>
2017-11-28 09:36:02 -0500

    worked on get request

--- client.py
+++ client.py
@@ -5,23 +5,27 @@ from bencodepy import decode_from_file, decode
 import hashlib
 import connection
 import struct
+import os
 
 class Client:
 
-	def __init__(self, ip_addr, port, filename, info_hash):
+	def __init__(self, ip_addr, port, filename, metainfo):
 
 		#list of peers (and connection info) that this client is connected to
 		self.connection_list = []
 		self.ip_addr = ip_addr
 		self.port = port
-		self.info_hash = info_hash
+		self.metainfo = metainfo
+		self.peer_id = os.urandom(20)
+
+		self.uploaded = 0
+		self.downloaded = 0
+		self.left = metainfo.size
+
 		self.check_for_file()
 		self.send_GET_request()
 		
-
-		
 		#from metainfo file
-		#self.peer_id = peer_id
 
 		#message = read/parse response from socket 
 		#from tracker reply to GET request
@@ -36,9 +40,13 @@ class Client:
 		#else if file is not in local directory run in leecher state
 
 	def send_GET_request(self):
-
+		get_request = bytearray("GET /announce?info_hash=")
+		get_request.extend(urllib.parse.quote_plus(metainfo.info_hash.digest.encode('utf-8')))
+		get_request.extend("&peer_id=")
+		get_request.extend(urllib.parse.quote_plus(self.peer_id.encode('utf-8')))
+		get_request.extend("&port=")
+		get_request.extend(self.port)
 		#get tracker information dictionary from metainfo file
-		#urlib.parse.quote_plus("string to url encode")
 		#info_hash, peer_id, port, uploaded, downloaed, left, compact, event, 
 		#send HTTP request to tracker	
 		return 0
@@ -63,7 +71,7 @@ class Client:
 		handshake = bytearray(b'\x18')
 		handshake.extend(map(ord, "URTorrent protocol"))
 		handshake.extend(bytearray(8))
-		handshake.extend(info_hash.digest())
+		handshake.extend(metainfo.info_hash.digest())
 		handshake.extend(map(ord, peer_id))
 		print(handshake)
 		print(len(handshake))
