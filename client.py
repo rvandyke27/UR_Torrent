5475e1c9e2eae5ec8290d57afdbdb1558637dbcc
rvandyke27 <rvandyke27@gmail.com>
2017-12-06 14:23:37 -0500

    got two peers to successfully transfer file

--- client.py
+++ client.py
@@ -89,10 +89,7 @@ class Client:
 				#if message is handshake
 				
 				if buf[0]==18 and buf[1:19] == b'URTorrent protocol' and buf[19:27] == b'\x00\x00\x00\x00\x00\x00\x00\x00' and buf[27:47] == self.info_hash.digest():
-				#if len(buf) > 0 and "URTorrent" in str(buf):
-					#ip = connection_socket.getsockname()[0]
-					#port = connection_socket.getsockname()[1]
-					#connection_socket.close()
+		
 					print("Received valid handshake", buf)
 
 					peer_connection.send(self.generate_handshake_msg())
@@ -132,7 +129,7 @@ class Client:
 
 			#split file into temporary chunks
 			f = open(self.filename, "rb")
-			for i in range(self.metainfo.num_pieces-2):
+			for i in range(self.metainfo.num_pieces-1):
 				temp_filename = "temp-" + str(i) + self.filename
 				fout = open(temp_filename, 'w+b')
 				b = f.read(65536)
@@ -238,23 +235,13 @@ class Client:
 
 	def initiate_handshaking(self, IP, port):
 		handshake_message = self.generate_handshake_msg()
-	#	new_connection = Connection(self, IP, port, BitArray(self.metainfo.num_pieces).set(False))
-		# temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		# temp_socket.connect((socket.gethostbyname(IP), port))
-		# ip = temp_socket.getsockname()[0]
-		# port = temp
 		new_connection = Connection(self, IP, port, BitArray(self.metainfo.num_pieces).set(False))
 		#send start of handshake
-	#	temp_socket.send(handshake_message)
 		new_connection.sock.send(handshake_message)
 		print("Handshake initiated ", handshake_message)
-
-		#just try to read hello message for now
-	#	handshake_response = temp_socket.recv(1024)
 		handshake_response = new_connection.sock.recv(1024)
 
 		#if handshake response is valid, save connection
-
 		if handshake_response[0]==18 and handshake_response[1:19] == b'URTorrent protocol' and handshake_response[19:27] == b'\x00\x00\x00\x00\x00\x00\x00\x00' and handshake_response[27:47] == self.info_hash.digest():			
 			new_connection.peer_id = handshake_response[47:68]
 			print("Received valid handshake response ", handshake_response)
@@ -264,14 +251,7 @@ class Client:
 			#listen for bitfield?
 
 
-		# new_connection, address = self.listening_socket.accept()
-		# buf = new_connection.sock.recv(1024)
-		# if len(buf) > 0:
-		# 	print("Received handshake response", buf)
-			#check if handshake is valid/good
-			#split off thread to listen for piece requests on this socket	
-
-		#listen for bitfield?
+		
 
 	def listen_to_peer(self, peer, address):
 		while True:
@@ -282,87 +262,76 @@ class Client:
 					#check messsage type
 					message_prefix = struct.unpack('>i', message[0:4])[0]
 					print("Message Prefix = ", message_prefix)
-					print(message_prefix==13)
 					message_id = message[4]
 					print("Message ID = ", message_id)
-					print(message_id == 6)
-					if message_prefix == 1:
-						if message_id == 0:
-							#choke
-							self.peer_choking == 1
-						elif message_id == 1:
-							#unchoke
-							self.peer_choking == 0
-						elif message_id == 2:
-							self.peer_interested == 1
-						elif message_id == 3:
-							self.peer_interested = 0
-						else:
-							print("Invalid Message")
-					elif message_prefix == 5:
-						#have message
-						print("Have Message")
-					elif message_prefix == 1 + len(self.peer_bitfield):
-						if message_id == 5:
-							#bitfield message
-							#check that bitfield is correct length
-							self.peer_bitfield = message[5:6+len(self.peer_bitfield)]
-							print(self.peer_bitfield)
-					elif message_prefix == 13:
+					if message_id == 6:
 						print("Request message received")
 						index = message[5:9]
 						begin = message[9:13]
 						length = message[13:17]
-						if message_id == 6:
-							#request message
-							#check that peer is not choked
-							#send requested chunk
-							length_prefix = 9 + 65536
-							piece_payload = bytearray(b'\x07')
-							piece_payload.extend(index)
-							piece_payload.extend(begin)
-							filename = "temp-" + str(int.from_bytes(index, byteorder='big')) + self.metainfo.filename
-							f = open(filename, 'rb')
-							b = bytearray()
-							b = f.read(65536)
-							if not b:
-								while True:
-									k = f.read(1)
-									if k:
-										b.extend(k)
-									else:
-										break
+						#request message
+						#check that peer is not choked
+						#send requested chunk
+						length_prefix = 9 + 65536
+						piece_payload = bytearray(b'\x07')
+						piece_payload.extend(index)
+						piece_payload.extend(begin)
+						print("Piece Message: ", piece_payload)
+						filename = "temp-" + str(int.from_bytes(index, byteorder='big')) + self.metainfo.filename
+						f = open(filename, 'rb')
+						b = bytearray()
+						b = f.read(65536)
+						if not b:
+							while True:
+								k = f.read(1)
+								if k:
+									b.extend(k)
+								else:
+									break
 
-							piece_payload.extend(b)
-							piece_message = bytearray(struct.pack('>i',len(piece_payload)))
-							piece_message.extend(piece_payload)
-							print("Piece Message: ", piece_message)
-							peer.send(piece_message)
+						piece_payload.extend(b)
+						piece_message = bytearray(struct.pack('>i',len(piece_payload)))
+						piece_message.extend(piece_payload)
+						peer.send(piece_message)
+						self.uploaded+=1
 					
-						elif message_id == 8:
+					elif message_id == 8:
 							#cancel message
 							print("Cancel Message")
-						else:
-							print("Invalid Message")
+					elif message_id == 0:
+						#choke
+						self.peer_choking == 1
+					elif message_id == 1:
+						#unchoke
+						self.peer_choking == 0
+					elif message_id == 2:
+						self.peer_interested == 1
+					elif message_id == 3:
+						self.peer_interested = 0
+					elif message_id == 4:
+						#have message
+						print("Have Message")
+					elif message_id == 5:
+						#bitfield message
+						#check that bitfield is correct length
+						self.peer_bitfield = message[5:6+len(self.peer_bitfield)]
+						print(self.peer_bitfield)
 					#default block size is 16384
-					elif message_prefix == 9 + 16384:
-						if message_id == 7:
-							#piece message
-							index = message[5:9]
-							begin = message[9:13]
-							piece = message[13:16397]
-							#save piece
-						else:
-							print("Invalid Message")
+					elif message_id == 7:
+						#piece message
+						index = message[5:9]
+						begin = message[9:13]
+						piece = message[13:]
+						#save piece	
 					else:
 						print("Invalid Message")
 
-					time.sleep(1)
+					time.sleep(.1)
 				else:
 					print("No data received")
 					time.sleep(1)
-			except:
-				print("Except")
+			except Exception as e:
+				print(e)
 				return False
 
 	def next_piece(self):
@@ -389,6 +358,29 @@ class Client:
 		handshake.extend(self.peer_id)
 		#print("Handshake message: ", handshake)
 		return handshake
+
+	def reassemble_file(self):
+		print("Reassembling File...")
+		f = open(self.filename, 'ab')
+
+		for i in range(self.metainfo.num_pieces):
+			piece_filename = "temp-" + str(i) + self.filename
+			piece = open(piece_filename, 'rb')
+			b = bytearray()
+			b = piece.read(65536)
+			if not b:
+				while True:
+					k = piece.read(1)
+					if k:
+						b.extend(k)
+					else:
+						break
+
+			f.write(b)
+
+		for fl in os.listdir():
+			if "temp" in fl:
+				os.remove(fl)
 
 	def exit_handler(self):
 		self.send_GET_request(2)
